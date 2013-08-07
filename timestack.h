429ce6a185f49edc36c05631b76eae1247bd3c8b
Drew Folta <drew@folta.net>
2013-08-06 18:22:52 -0700

    added reference to license in the header

--- timestack.h
+++ timestack.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2013 Drew Folta.  All rights reserved.
+ * Copyrights licensed under the MIT License.
+ * See the accompanying LICENSE.txt file for terms.
+ */
+
 /**********************************************************************
  * TIMESTACK
  */
